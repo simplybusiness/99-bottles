176ea21461a8d27980b34735196e87f4fbf19db3
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-02-14 10:18:48 +0000

    Removed the if-else.

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -13,16 +13,6 @@ class BottlesOfBeerSongTest < Minitest::Test
       "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
         "Take one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
     end
-    if number == 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\n" +
-        "Take it down and pass it around, no more bottles of beer on the wall.\n"
-    elsif number == 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\n" +
-        "Take one down and pass it around, 1 bottle of beer on the wall.\n"
-    else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
-        "Take one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
-    end
   end
 
   def test_verse_1
