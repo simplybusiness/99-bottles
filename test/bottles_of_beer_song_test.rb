920d0a9854327acefc72959a39c670f6901b2e9a
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-02-24 15:26:57 +0000

    Removed duplication.

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -9,9 +9,6 @@ class BottlesOfBeerSongTest < Minitest::Test
     when 1
       "1 bottle of beer on the wall, 1 bottle of beer.\n" +
         "Take it down and pass it around, no more bottles of beer on the wall.\n"
-    when 2
-      "#{number} #{bottle_amount(number)} of beer on the wall, #{number} #{bottle_amount(number)} of beer.\n" +
-        "Take one down and pass it around, #{number - 1} #{bottle_amount(number - 1)} of beer on the wall.\n"
     else
       "#{number} #{bottle_amount(number)} of beer on the wall, #{number} #{bottle_amount(number)} of beer.\n" +
         "Take one down and pass it around, #{number - 1} #{bottle_amount(number - 1)} of beer on the wall.\n"
