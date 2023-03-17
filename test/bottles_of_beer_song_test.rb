fe031dcc586f7a1d9704fae421c0bb12e98e720d
Stanton Borthwick <stanton.borthwick@simplybusiness.co.uk>
2023-03-17 13:49:02 +0000

    Refactored name

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -45,8 +45,9 @@ class BottlesOfBeerSongTest < Minitest::Test
 
   def verse(number_of_bottles)
     bottles_song = "#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.\n"
-    bottle_name = number_of_bottles-1 == 1 ? 'bottle' : 'bottles'
+    number_of_bottles_remaining = number_of_bottles - 1
+    bottle_name = number_of_bottles_remaining == 1 ? 'bottle' : 'bottles'
 
-    bottles_song + "Take one down and pass it around, #{number_of_bottles-1} #{bottle_name} of beer on the wall.\n"
+    bottles_song + "Take one down and pass it around, #{number_of_bottles_remaining} #{bottle_name} of beer on the wall.\n"
   end
 end
