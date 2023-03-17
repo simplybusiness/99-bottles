d948c36425de86507f7882145c61dc927ed9406d
Mitchell Perry <mitchell.perry@simplybusiness.co.uk>
2023-03-17 13:13:42 +0000

    can display string with a given number of bottles

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -10,7 +10,6 @@ class BottlesOfBeerSongTest < Minitest::Test
   end
 
   def test_verse_2
-    skip('Test list')
     expected_verse =
       "98 bottles of beer on the wall, 98 bottles of beer.\n" +
         "Take one down and pass it around, 97 bottles of beer on the wall.\n"
@@ -46,7 +45,7 @@ class BottlesOfBeerSongTest < Minitest::Test
   end
 
   def verse(number_of_bottles)
-    "99 bottles of beer on the wall, 99 bottles of beer.\n" +
-    "Take one down and pass it around, 98 bottles of beer on the wall.\n"
+    "#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.\n" +
+    "Take one down and pass it around, #{number_of_bottles-1} bottles of beer on the wall.\n"
   end
 end
