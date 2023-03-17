4857bb735f2b69fe0fa5f4d107bf8b7985d9642b
Mitchell Perry <mitchell.perry@simplybusiness.co.uk>
2023-03-17 13:27:27 +0000

    made similar code more similar

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -45,8 +45,8 @@ class BottlesOfBeerSongTest < Minitest::Test
 
   def verse(number_of_bottles)
     if number_of_bottles == 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\n" +
-        "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+      "#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.\n" +
+        "Take one down and pass it around, #{number_of_bottles-1} bottle of beer on the wall.\n"
     else
       "#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.\n" +
       "Take one down and pass it around, #{number_of_bottles-1} bottles of beer on the wall.\n"
