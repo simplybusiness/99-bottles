64baed7b57fe23c08c1fb2ceb58b3a0d4d8f3356
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-04-12 14:06:54 +0100

    Replaced the 2 string with a number interpolated in the verse.

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -10,7 +10,7 @@ class BottlesOfBeerSongTest < Minitest::Test
       "1 bottle of beer on the wall, 1 bottle of beer.\n" +
         "Take it down and pass it around, no more bottles of beer on the wall.\n"
     when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\n" +
+      "#{2} bottles of beer on the wall, #{2} bottles of beer.\n" +
         "Take one down and pass it around, 1 bottle of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
