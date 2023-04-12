5484a37b291e1cd2b0d5dcb9ffd4c7a6665bc25f
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-04-12 14:56:12 +0100

    Made Similar Code Even More Similar (quantity starts with capital letter).

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -4,10 +4,10 @@ class BottlesOfBeerSongTest < Minitest::Test
   def verse(number)
     case number
     when 0
-      "No more bottles of beer on the wall, no more bottles of beer.\n" +
+      "#{quantity(number).capitalize} #{container(number)} of beer on the wall, #{quantity(number)} #{container(number)} of beer.\n" +
         "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     else
-      "#{quantity(number)} #{container(number)} of beer on the wall, #{quantity(number)} #{container(number)} of beer.\n" +
+      "#{quantity(number).capitalize} #{container(number)} of beer on the wall, #{quantity(number)} #{container(number)} of beer.\n" +
         "Take #{pronoun(number)} down and pass it around, #{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"
     end
   end
