1e404046857b84fe44bb2d7f1b8af8a0c2dd2da5
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-04-12 14:58:32 +0100

    Identified a new abstraction (what action to take based on the number of bottles).

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -8,7 +8,7 @@ class BottlesOfBeerSongTest < Minitest::Test
         "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     else
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, #{quantity(number)} #{container(number)} of beer.\n" +
-        "Take #{pronoun(number)} down and pass it around, #{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"
+        "#{action(number)}, #{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"
     end
   end
 
@@ -26,6 +26,10 @@ class BottlesOfBeerSongTest < Minitest::Test
     else
       "bottles"
     end
+  end
+
+  def action(number)
+    "Take #{pronoun(number)} down and pass it around"
   end
 
   def pronoun(number)
