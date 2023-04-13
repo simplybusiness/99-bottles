b9fcb452215172aa2b81194daa021799990287ef
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-04-13 10:02:13 +0100

    We do not need to use the private method in the client.

--- lib/bottle_verse.rb
+++ lib/bottle_verse.rb
@@ -4,8 +4,8 @@ class BottleVerse
   def verse(number)
     number_of_bottles = Bottles.for_verse(number)
     next_verse = number_of_bottles.successive
-    "#{number_of_bottles.quantity.capitalize} #{container(number_of_bottles)} of beer on the wall, #{number_of_bottles.quantity} #{container(number_of_bottles)} of beer.\n" +
-      "#{action(number_of_bottles)}, #{next_verse.quantity} #{container(next_verse)} of beer on the wall.\n"
+    "#{number_of_bottles.quantity.capitalize} #{number_of_bottles.container} of beer on the wall, #{number_of_bottles.quantity} #{number_of_bottles.container} of beer.\n" +
+      "#{action(number_of_bottles)}, #{next_verse.quantity} #{next_verse.container} of beer on the wall.\n"
   end
 
   private
