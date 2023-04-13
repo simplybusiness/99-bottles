c85ed05892e2ccd65024f654333847ffd9692568
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-04-13 10:03:15 +0100

    Inlined the action method.

--- lib/bottle_verse.rb
+++ lib/bottle_verse.rb
@@ -5,12 +5,6 @@ class BottleVerse
     number_of_bottles = Bottles.for_verse(number)
     next_verse = number_of_bottles.successive
     "#{number_of_bottles.quantity.capitalize} #{number_of_bottles.container} of beer on the wall, #{number_of_bottles.quantity} #{number_of_bottles.container} of beer.\n" +
-      "#{action(number_of_bottles)}, #{next_verse.quantity} #{next_verse.container} of beer on the wall.\n"
-  end
-
-  private
-
-  def action(number)
-    number.action
+      "#{number_of_bottles.action}, #{next_verse.quantity} #{next_verse.container} of beer on the wall.\n"
   end
 end
