4efcc1c5f2da3171364a1a088503bfa55db2ea5b
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-04-13 09:57:31 +0100

    Migrated a client to use the factory method in Bottles.

--- lib/bottle_verse.rb
+++ lib/bottle_verse.rb
@@ -2,7 +2,7 @@ require_relative './no_bottles'
 require_relative './bottles'
 class BottleVerse
   def verse(number)
-    number_of_bottles = bottles(number)
+    number_of_bottles = Bottles.for_verse(number)
     next_verse = number_of_bottles.successive
     "#{quantity(number_of_bottles).capitalize} #{container(number_of_bottles)} of beer on the wall, #{quantity(number_of_bottles)} #{container(number_of_bottles)} of beer.\n" +
       "#{action(number_of_bottles)}, #{quantity(next_verse)} #{container(next_verse)} of beer on the wall.\n"
