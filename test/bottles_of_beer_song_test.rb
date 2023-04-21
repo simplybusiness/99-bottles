a44e115ac82d7ed8b94e2ec418021e401cadb716
Doug Grainger <doug.grainger@simplybusiness.com>
2023-04-21 11:15:29 -0400

    introduce next bottle count method

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -4,19 +4,23 @@ class BottlesOfBeerSongTest < Minitest::Test
   def verse(number)
     case number
     when 0
-      "No more bottles of beer on the wall, no more bottles of beer.\n" +
-        "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+      "No more #{pluralize_bottle(number)} of beer on the wall, no more #{pluralize_bottle(number)} of beer.\n" +
+        "Go to the store and buy some more, #{next_bottle_count(number)} #{pluralize_bottle(next_bottle_count(number))} of beer on the wall.\n"
     when 1
       "#{number} #{pluralize_bottle(number)} of beer on the wall, #{number} #{pluralize_bottle(number)} of beer.\n" +
-        "Take it down and pass it around, no more #{pluralize_bottle(number-1)} of beer on the wall.\n"
+        "Take it down and pass it around, no more #{pluralize_bottle(next_bottle_count(number))} of beer on the wall.\n"
     else
       "#{number} #{pluralize_bottle(number)} of beer on the wall, #{number} #{pluralize_bottle(number)} of beer.\n" +
-        "Take one down and pass it around, #{number - 1} #{pluralize_bottle(number-1)} of beer on the wall.\n"
+        "Take one down and pass it around, #{next_bottle_count(number)} #{pluralize_bottle(next_bottle_count(number))} of beer on the wall.\n"
     end
   end
 
   def pluralize_bottle(number)
     number == 1 ? "bottle" : "bottles"
+  end
+
+  def next_bottle_count(number)
+    number == 0 ? 99 : (number-1)
   end
 
   def test_verse_1
