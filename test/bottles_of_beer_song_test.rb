4a26258e2e474136cb78fa58ec5e93c0eec9d223
Doug Grainger <doug.grainger@simplybusiness.com>
2023-04-21 11:20:35 -0400

    introduce bottle qty method

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -4,10 +4,10 @@ class BottlesOfBeerSongTest < Minitest::Test
   def verse(number)
     case number
     when 0
-      "No more #{pluralize_bottle(number)} of beer on the wall, no more #{pluralize_bottle(number)} of beer.\n" +
+      "#{bottle_qty(number).capitalize} #{pluralize_bottle(number)} of beer on the wall, #{bottle_qty(number)} #{pluralize_bottle(number)} of beer.\n" +
         "Go to the store and buy some more, #{next_bottle_count(number)} #{pluralize_bottle(next_bottle_count(number))} of beer on the wall.\n"
     when 1
-      "#{number} #{pluralize_bottle(number)} of beer on the wall, #{number} #{pluralize_bottle(number)} of beer.\n" +
+      "#{bottle_qty(number).capitalize} #{pluralize_bottle(number)} of beer on the wall, #{number} #{pluralize_bottle(number)} of beer.\n" +
         "Take it down and pass it around, no more #{pluralize_bottle(next_bottle_count(number))} of beer on the wall.\n"
     else
       "#{number} #{pluralize_bottle(number)} of beer on the wall, #{number} #{pluralize_bottle(number)} of beer.\n" +
@@ -21,6 +21,10 @@ class BottlesOfBeerSongTest < Minitest::Test
 
   def next_bottle_count(number)
     number == 0 ? 99 : (number-1)
+  end
+
+  def bottle_qty(number)
+    number == 0 ? "no more" : number.to_s
   end
 
   def test_verse_1
